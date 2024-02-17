cb84ff8fdcb3abf079a390e2fbbef9b5f02365a4
barron <asd709380942@gmail.com>
2024-02-17 10:13:19 +0800

    feat: 在react中處理多個input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstName: "", lastName: "" });
   return (
     <div>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstName}
+        />
+        <input
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          type="text"
+          value={name.lastName}
         />
       </form>
     </div>
